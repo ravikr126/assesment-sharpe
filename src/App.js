4cf90856c28e05781e3328164fff503ee274afe3
Ravi Kumar <ravi14577@gmail.com>
2023-09-30 20:22:13 +0530

    adding 2nd assesment code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import ChartComponent from "./ChartComponent"
+import DelayedInput from './DelayedInput';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <h1>Delayed Input Example</h1>
+      <DelayedInput />
+
+
       <ChartComponent />
     </div>
   );
